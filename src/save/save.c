4e54201f6cb82b5fd8ddc8ef594e2b58d25c7c87
MyName <fork1oom@yandex.com>
2025-07-29 08:20:15 +0300

    libsave: Allow loading of moo13 save format

--- src/save/save.c
+++ src/save/save.c
@@ -76,6 +76,9 @@ static int game_save_check_saves(void)
 
 int libsave_do_load_smart(const char *filename, struct game_s *g)
 {
+    if (savetype_is_moo13(g, filename) && !savetype_de_moo13(g, filename)) {
+        return 0;
+    }
     return libsave_1oom_do_load(filename, g, -1, NULL);
 }
 
